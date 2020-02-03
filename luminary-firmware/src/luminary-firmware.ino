e3e50c6e7a8e083e18f7c00ea26a74c20090ef6d
Kevin Conner <kevin.r.conner@gmail.com>
2020-02-03 10:04:40 -0800

    firmware - basic clock functionality established

---
+++ luminary-firmware/src/luminary-firmware.ino
@@ -0,0 +1,74 @@
+/*
+ * Project luminary-firmware
+ * Description: Firmware for running "Luminary" pseudo-analog clock
+ * Author: Kevin Conner
+ * Date: 2020JAN31
+ */
+
+#include "Particle.h"
+#include "neopixel.h"
+
+#define PIXEL_COUNT 60
+#define PIXEL_PIN D7
+#define PIXEL_TYPE WS2812B
+
+Adafruit_NeoPixel pixels(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);	
+
+// particle cloud variables //
+int timezone = -8;
+int hand_hour_color = 0xff0000;
+int hand_minute_color = 0x00ff00;
+int hand_second_color = 0x0000ff;
+
+
+// setup() runs once, when the device is first turned on.
+void setup() {
+  
+  Particle.variable("timezone", timezone);
+  Particle.variable("hand_hour_color", hand_hour_color);
+  Particle.variable("hand_minute_color", hand_minute_color);
+  Particle.variable("hand_second_color", hand_second_color);
+
+  pixels.begin();
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() { 
+  
+  Time.zone(timezone);
+
+  pixels.clear();
+
+  int hourHand = ((Time.hour()%12)*5)+int(Time.minute()/12);
+  int minuteHand = Time.minute();
+  int secondHand = Time.second();
+  pixels.setPixelColor(hourHand,255,0,0);
+  pixels.setPixelColor(minuteHand,0,255,0);
+  pixels.setPixelColor(secondHand,0,0,255);
+
+  // setPixelColorHex(pixels, hourHand, hand_hour_color);
+  // setPixelColorHex(pixels, minuteHand, hand_minute_color);
+  // setPixelColorHex(pixels, secondHand, hand_second_color);
+
+  pixels.show();
+
+}
+
+void setTimezone(){
+
+}
+
+void setHandColors(){
+
+}
+
+void setPixelColorHex(Adafruit_NeoPixel neo, int pixelIndex, int hexColor){
+
+  uint8_t r = (hexColor & 0xff0000 ) >> 16;
+  uint8_t g = (hexColor & 0x00ff00 ) >> 8;
+  uint8_t b = (hexColor & 0x0000ff );
+
+  neo.setPixelColor(pixelIndex,r,g,b);
+
+}
